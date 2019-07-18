62d20bc6755849b6bd0cf1bf7e7f14426be0cd5d
Herb Caudill <herb@devresults.com>
2019-07-18 22:52:01 +0200

    stories/index.stories: S/M/L; scrabble attacks

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -8,9 +8,34 @@ import { Tile } from '../src/components/Tile'
 import { Word } from '../src/components/Word'
 
 storiesOf('Tile', module)
-  .add('A', () => <Tile letter="A" size="500" />)
-  .add('Q', () => <Tile letter="Q" size="75" />)
+  .add('Small', () => (
+    <div>
+      <Tile letter="A" size={75} seed="SA" />
+      <Tile letter="J" size={75} seed="SJ" />
+      <Tile letter="Q" size={75} seed="SQ" />
+      <Tile letter="Q" size={75} seed="SQ" isFaceUp={false} />
+    </div>
+  ))
+  .add('Medium', () => (
+    <div>
+      <Tile letter="A" size={150} seed="MA" />
+      <Tile letter="J" size={150} seed="MJ" />
+      <Tile letter="Q" size={150} seed="MQ" />
+      <Tile letter="Q" size={150} seed="MQ" isFaceUp={false} />
+    </div>
+  ))
+  .add('Large', () => (
+    <div>
+      <Tile letter="A" size={300} seed="LA" />
+      <Tile letter="J" size={300} seed="LJ" />
+      <Tile letter="Q" size={300} seed="LQ" />
+      <Tile letter="Q" size={300} seed="LQ" isFaceUp={false} />
+    </div>
+  ))
 
-storiesOf('Word', module)
-  .add('Scrabble', () => <Word word="SCRABBLE" size="150" />)
-  .add('Attacks', () => <Word word="ATTACKS" size="50" />)
+storiesOf('Word', module).add('Scrabble Attacks', () => (
+  <div>
+    <Word word="SCRABBLE" size={50} />
+    <Word word="ATTACKS" size={75} />
+  </div>
+))
