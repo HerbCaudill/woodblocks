09a1a5b3c12cb3cf50bda69b8415654503e4ac4f
Herb Caudill <herb@devresults.com>
2019-07-29 22:17:56 -0600

    Board.test.ts:

--- src/Board.test.ts
+++ src/Board.test.ts
@@ -1,7 +1,12 @@
 ﻿import { Board } from './Board'
 import { trim } from './trim'
+import { pieces } from 'Piece'
 
-const EMPTY_BOARD = trim(`
+const match = (a: Board, b: string) => {
+  expect(a.toString()).toEqual(trim(b))
+}
+
+const emptyBoard = trim(`
   ··········
   ··········
   ··········
@@ -29,15 +34,147 @@ describe('Board', () => {
       expect(boardIsEmpty(b)).toBe(true)
     })
 
-    describe('toString', () => {
-      it('should render an empty board correctly ', () => {
-        expect(new Board().toString()).toEqual(EMPTY_BOARD)
-      })
+    it('should construct an empty board correctly', () => {
+      const b = new Board(emptyBoard)
+      expect(boardIsEmpty(b)).toBe(true)
     })
 
-    it('should construct an empty board correctly', () => {
-      const b = new Board(EMPTY_BOARD)
-      expect(boardIsEmpty(b)).toBe(true)
+    it('should construct from a given string', () => {
+      const board = new Board(`
+         ■·········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········`)
+      expect(boardIsEmpty(board)).toBe(false)
+      expect(board[0][0]).toBe(true)
+      expect(board[1][0]).toBe(false)
+      expect(board[0][1]).toBe(false)
+      expect(board[1][1]).toBe(false)
+    })
+  })
+
+  describe('toString', () => {
+    it('should render an empty board correctly ', () => {
+      expect(new Board().toString()).toEqual(emptyBoard)
+    })
+  })
+
+  describe('conflicts', () => {
+    test('case 1', () => {
+      const board = new Board(`
+         ■·········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········`)
+      expect(board.conflictsExist(pieces[0], [0, 0])).toBe(true)
+      expect(board.conflictsExist(pieces[0], [1, 0])).toBe(false)
+    })
+
+    test('case 2', () => {
+      const board = new Board(`
+         ··········
+         ·■········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········`)
+      expect(board.conflictsExist(pieces[9], [0, 0])).toBe(false)
+      expect(board.conflictsExist(pieces[9], [1, 0])).toBe(true)
+    })
+  })
+
+  describe('pieceFits', () => {
+    it('1x1', () => {
+      const board = new Board()
+      expect(board.pieceFits(pieces[0], [0, 0])).toBe(true)
+      expect(board.pieceFits(pieces[0], [11, 0])).toBe(false)
+      expect(board.pieceFits(pieces[0], [0, 11])).toBe(false)
+    })
+
+    it('5x1', () => {
+      const board = new Board()
+      expect(board.pieceFits(pieces[4], [0, 0])).toBe(true)
+      expect(board.pieceFits(pieces[4], [8, 0])).toBe(false)
+      expect(board.pieceFits(pieces[4], [0, 8])).toBe(true)
+    })
+
+    it('1x5', () => {
+      const board = new Board()
+      expect(board.pieceFits(pieces[8], [0, 0])).toBe(true)
+      expect(board.pieceFits(pieces[8], [8, 0])).toBe(true)
+      expect(board.pieceFits(pieces[8], [0, 8])).toBe(false)
+    })
+  })
+
+  describe('addPiece', () => {
+    test('case 1', () => {
+      const board = new Board(emptyBoard)
+      board.addPiece(pieces[0], [0, 0])
+      match(
+        board,
+        `■·········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········`
+      )
+    })
+
+    test('case 2', () => {
+      const board = new Board(emptyBoard)
+      board.addPiece(pieces[5], [2, 0])
+      match(
+        board,
+        `··········
+         ··········
+         ■·········
+         ■·········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········`
+      )
+    })
+
+    test('case 2', () => {
+      const board = new Board(emptyBoard)
+      board.addPiece(pieces[10], [3, 3])
+      match(
+        board,
+        `··········
+         ··········
+         ··········
+         ···■■·····
+         ····■·····
+         ··········
+         ··········
+         ··········
+         ··········
+         ··········`
+      )
     })
   })
 })
