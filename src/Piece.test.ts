b047d0e77c1f53ac876641c7c4b02037ac314b4b
Herb Caudill <herb@devresults.com>
2019-07-29 20:08:55 -0600

    Piece.test.ts:

--- src/Piece.test.ts
+++ src/Piece.test.ts
@@ -1,9 +1,13 @@
-﻿import { Piece } from './Piece'
+﻿import { pieces } from './Piece'
 
-describe('Piece', () => {
+describe('pieces', () => {
   describe('constructor', () => {
     it('should ', () => {
-      expect(new Piece())
+      expect(pieces[0]).toEqual([[true]])
+      expect(pieces[1]).toEqual([[true, true]])
+      expect(pieces[5]).toEqual([[true], [true]])
+      expect(pieces[10]).toEqual([[true, true], [false, true]])
+      expect(pieces[15]).toEqual([[false, false, true], [false, false, true], [true, true, true]])
     })
   })
 })
