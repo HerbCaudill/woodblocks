485bae7c709e891fd7cf5d68f1af0fbbb16a00ee
Herb Caudill <herb@devresults.com>
2019-07-30 16:43:38 -0400

    stories/index.stories.js: add Piece stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,7 +4,9 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
 import { Board } from '../src/models/Board'
+import { pieces } from '../src/models/pieces'
 import { Gameboard } from '../src/components/Gameboard'
+import { Piece } from '../src/components/Piece'
 
 const b0 = new Board()
 
@@ -52,3 +54,7 @@ storiesOf('Gameboard', module)
   .add('one piece', () => <Gameboard board={b1} />)
   .add('many pieces', () => <Gameboard board={b2} />)
   .add('full', () => <Gameboard board={b3} />)
+
+for (const piece in pieces) {
+  storiesOf('Piece', module).add(piece, () => <Piece size={20} name={piece} />)
+}
