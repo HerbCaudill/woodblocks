8b09f1652c33bbc602b11244277a861ae1ab88e7
Herb Caudill <herb@devresults.com>
2019-07-30 16:44:54 -0400

    components/Game.tsx: create

---
+++ src/components/Game.tsx
@@ -0,0 +1,25 @@
+﻿/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import { Board } from 'models/Board'
+import { Gameboard } from './Gameboard'
+
+interface GameProps {
+  board: Board
+}
+
+export const Game = ({ board }: GameProps) => {
+  const styles = getStyles()
+  return (
+    <div css={styles.game}>
+      <Gameboard board={board} size={50} />
+    </div>
+  )
+}
+
+const getStyles = () => {
+  return {
+    game: css({
+      padding: 2,
+    }),
+  }
+}
