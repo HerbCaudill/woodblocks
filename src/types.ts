4723573da72e245ffd5ec5141b9138aaeb2f9b91
Herb Caudill <herb@devresults.com>
2019-08-20 19:36:03 +0200

    types.ts:

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+import { Piece } from 'models/Piece'
+import { Board } from 'models/Board'
+
+export interface Cell {
+  filled: boolean
+  hover?: boolean
+}
+
+export type Line = Cell[]
+export type Layout = Line[]
+export type Position = [number, number]
+
+export type Move = [Piece, Position]
+export type Situation = { board: Board; pieces: Piece[] }
+export type Strategy = ({ board, pieces }: Situation) => Move
+export type StrategyBook = {
+  [key: string]: Strategy
+}
