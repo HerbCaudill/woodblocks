2091f598ee6695c421e48113aa70fc964f5eb288
Herb Caudill <herb@devresults.com>
2019-08-20 19:36:18 +0200

    strategies/random.ts: create

---
+++ src/strategies/random.ts
@@ -0,0 +1,14 @@
+﻿import { Strategy, Position } from '../types'
+
+const random: Strategy = ({ board, pieces }) => {
+  // find the first piece that has at least one legal move
+  const piece = pieces.find(p => board.allowedPositions(p).length > 0)
+  if (piece === undefined) throw new Error('Game over')
+
+  const allowedPositions = board.allowedPositions(piece)
+  // pick one at random
+  const position = allowedPositions![Math.floor(Math.random() * allowedPositions.length)]
+  return [piece, position]
+}
+
+export default random
